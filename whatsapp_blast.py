bd377ea668e85563c893d18c7be39796109ffde0
Muh. Fachrul Islam Sukimin <106242826+kiminfachrul@users.noreply.github.com>
2024-08-12 17:29:10 +0800

    Update label teks whatsapp_blast.py
    -Menambahkan label teks untuk dapat di panggil di bagian akhir pesan

--- whatsapp_blast.py
+++ whatsapp_blast.py
@@ -23,36 +23,28 @@ driver.get('https://web.whatsapp.com/')
 # input("Scan QR code dan tekan Enter setelah login...")
 
 # Fungsi untuk mengirim pesan
-def kirim_pesan(nomor, nama, pesan):
+def kirim_pesan(nomor_wa, nama, pesan):
     # Buka chat
-    driver.get(f'https://web.whatsapp.com/send?phone={nomor}&text={pesan}')
+    driver.get(f'https://web.whatsapp.com/send?phone={nomor_wa}&text={pesan}')
     
     try:
-        # search_box = WebDriverWait(driver, 20).until(
-        #     EC.presence_of_element_located((By.XPATH, '//div[@data-testid="chat-list-search"]//p[@contenteditable="true"]'))
-        # )
-        # search_box.clear()
-        # search_box.send_keys(nomor)
-        # search_box.send_keys(Keys.ENTER)
-
-        # Tunggu hingga pesan terisi di kotak chat
-        WebDriverWait(driver, 20).until(
+        WebDriverWait(driver, 14).until(
             EC.presence_of_element_located((By.XPATH, '//div[@class="x1hx0egp x6ikm8r x1odjw0f x1k6rcq7 x6prxxf"][@data-tab="10"]'))
         )
     
-        time.sleep(3)
-        
+        time.sleep(2)
+
         # Tekan tombol Enter untuk mengirim pesan
         message_box = driver.find_element(By.XPATH, '//div[@class="x1hx0egp x6ikm8r x1odjw0f x1k6rcq7 x6prxxf"][@data-tab="10"]')
         message_box.send_keys(pesan)
         message_box.send_keys(Keys.ENTER)
 
-        print(f"Pesan berhasil dikirim ke {nama} ({nomor})")
+        print(f"Pesan berhasil dikirim ke {nama} ({nomor_wa})")
         
-        time.sleep(3) 
+        time.sleep(2) 
         
     except Exception as e:
-        print(f"Gagal mengirim pesan ke {nama} ({nomor}): {str(e)}")
+        print(f"Gagal mengirim pesan ke {nama} ({nomor_wa}): {str(e)}")
 
 # disini fungsi loop di eksekusi
 for index, row in df.head(2).iterrows():
@@ -60,10 +52,11 @@ for index, row in df.head(2).iterrows():
     nama = row['nama']
     nomor_wa = row['nomor_wa']
     status = row['status']
+    teks_pesan = row['teks']
     
     # Buat pesan
-    pesan = f"HELLO!! {nama}, ini adalah pesan otomatis untuk pelanggan dengan ID: {id_pelanggan}. Status Anda saat ini adalah: {status}"
-    
+    pesan = f"HELLO!! {nama}, ini adalah pesan otomatis untuk pelanggan dengan ID: {id_pelanggan}. Status Anda saat ini adalah: {status}. \n {teks_pesan}"
+
     try:
         kirim_pesan(nomor_wa, nama, pesan)
     except Exception as e:
